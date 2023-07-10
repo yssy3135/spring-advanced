d4715640ae609b667b88a25122ea27790cdec01e
yssy3135 <ys3135@gmail.com>
2023-07-10 23:28:51 +0900

    ADD, TemplateMethod 적용

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -50,5 +50,26 @@ public class TemplateMethodTest {
         template2.execute();
     }
 
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        template2.execute();
+    }
+
+
 
 }
