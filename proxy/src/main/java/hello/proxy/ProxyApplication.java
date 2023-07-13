d6a5f7228b59515926a12ce0b81543a2e75ec58d
yssy3135 <ys3135@gmail.com>
2023-07-14 00:23:29 +0900

    MOD, 모듈 자바버전 오류 수정.., 빈 수동등록 예제추가

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,11 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(AppV1config.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
