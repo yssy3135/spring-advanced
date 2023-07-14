d99cbbef271c6cdaa9ad0319f8da1939e626995a
yssy3135 <ys3135@gmail.com>
2023-07-14 17:29:21 +0900

    ADD, 프록시 패턴 예제 추가

---
+++ proxy/src/main/java/hello/proxy/app/v3/OrderRepositoryV3.java
@@ -0,0 +1,26 @@
+package hello.proxy.app.v3;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Repository
+public class OrderRepositoryV3 {
+
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ proxy/src/test/java/hello/proxy/qureproxy/proxy/code/ProxyPatternClient.java
@@ -0,0 +1,15 @@
+package hello.proxy.qureproxy.proxy.code;
+
+public class ProxyPatternClient {
+
+    private Subject subject;
+
+    public ProxyPatternClient(Subject subject) {
+        this.subject = subject;
+    }
+
+    public void execute() {
+        subject.operation();
+    }
+
+}
