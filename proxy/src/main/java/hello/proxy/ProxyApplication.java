202d22cd0a810e043aa8484abcd506d6ad441437
yssy3135 <ys3135@gmail.com>
2023-07-14 15:24:14 +0900

    ADD, v2예제 추가

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,12 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1config;
+import hello.proxy.config.AppV2config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1config.class)
+@Import({AppV1config.class,AppV2config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
