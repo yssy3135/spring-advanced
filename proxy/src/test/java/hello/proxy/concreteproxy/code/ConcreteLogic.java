47e70225ff8a8452327184a97459efb8fbd33741
yssy3135 <ys3135@gmail.com>
2023-07-16 00:56:11 +0900

    ADD, 구체 클래스 기반 프록시 적용

---
+++ proxy/src/test/java/hello/proxy/concreteproxy/code/ConcreteLogic.java
@@ -0,0 +1,15 @@
+package hello.proxy.concreteproxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConcreteLogic {
+
+    public String operation() {
+        log.info("ConcreteLogin 실행");
+        return "data";
+    }
+
+
+
+}
