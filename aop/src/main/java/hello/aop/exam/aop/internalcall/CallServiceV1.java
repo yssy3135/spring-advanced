ff9f7271dedf5b36c64e1ad535a8f13e1d92d998
yssy3135 <ys3135@gmail.com>
2023-07-23 19:12:25 +0900

    @Test ADD, 내부호출 대안1 자기 자신 주입

---
+++ aop/src/main/java/hello/aop/exam/aop/internalcall/CallServiceV1.java
@@ -0,0 +1,31 @@
+package hello.aop.exam.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+    private CallServiceV1 callServiceV1;
+
+
+    //setter(수정자) 의존관계 주입을 하면 스프링 빈이 생성된 이후에 주입할 수 있기 때문에 오류가 발생하지 않는다.
+    @Autowired
+    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+        log.info("callServiceV1 setter={}", callServiceV1.getClass());
+        this.callServiceV1 = callServiceV1;
+    }
+
+    public void external() {
+        log.info("call external");
+        callServiceV1.internal(); // 외부 메서드 호출
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+
+
+}
