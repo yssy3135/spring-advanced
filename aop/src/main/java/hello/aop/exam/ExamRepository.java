3abe3a4bf9f2fc812088336063081bbc17eb46d7
yssy3135 <ys3135@gmail.com>
2023-07-23 16:04:38 +0900

    ADD, 로그출럭 aop 추가

---
+++ aop/src/main/java/hello/aop/exam/ExamRepository.java
@@ -0,0 +1,25 @@
+package hello.aop.exam;
+
+import org.springframework.stereotype.Repository;
+
+import java.lang.annotation.Retention;
+
+@Repository
+public class ExamRepository {
+
+    private static int seq = 0;
+
+    /**
+     * 5번에 1번 실패하는 요청
+     */
+
+    public String save(String itemId) {
+        seq++;
+        if(seq % 5 == 0) {
+            throw new IllegalStateException("예외발생");
+        }
+        return "ok";
+    }
+
+
+}
