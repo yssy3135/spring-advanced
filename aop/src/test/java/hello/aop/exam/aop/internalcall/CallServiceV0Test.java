fcb90e25f607d154e153436fd36587dbd69fe9fa
yssy3135 <ys3135@gmail.com>
2023-07-23 19:01:16 +0900

    ADD, 내부호출 문제점 예제

---
+++ aop/src/test/java/hello/aop/exam/aop/internalcall/CallServiceV0Test.java
@@ -0,0 +1,30 @@
+package hello.aop.exam.aop.internalcall;
+
+import hello.aop.exam.aop.internalcall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV0Test {
+
+    @Autowired
+    CallServiceV0 callServiceV0;
+
+    @Test
+    void external() {
+//        log.info("target={}", callServiceV0.getClass());
+        callServiceV0.external();
+    }
+
+    @Test
+    void internal() {
+        callServiceV0.internal();
+    }
+}
